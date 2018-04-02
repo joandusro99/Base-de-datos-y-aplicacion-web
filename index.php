f0ddfc61813428494caeb494100d3d33ad49b626
Estudiantes <Estudiantes@Labing50721.udistrital.edu.co>
2018-04-02 18:50:08 -0500

    primero

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>loginUD</title>
+    </head>
+    <body>
+        <p class="titulo">
+            login
+        
+        </p>
+        <form action="procesador.php">
+            <input type="text" name="usuario"><br>
+            <input type="password" name="clave"><br>
+            <input type="submit" name="boton"><br>
+                
+        </form>
+       
+    </body>
+</html>
---
+++ procesador.php
@@ -0,0 +1,9 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/* @var $_REQUEST type */
+echo $_REQUEST ["clave"]+ $_REQUEST["usuario"];
